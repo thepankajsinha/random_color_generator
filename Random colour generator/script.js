4be1a85e7803798539bb42068089ca11569827f8
thepankajsinha <136917326+thepankajsinha@users.noreply.github.com>
2024-04-17 10:08:13 +0530

    Add files via upload

---
+++ Random colour generator/script.js
@@ -0,0 +1,18 @@
+const getColor = () => {
+
+    const randomNumber = Math.floor(Math.random() * 16777215)
+    const randomCode = "#" +  randomNumber.toString(16);
+
+    document.body.style.backgroundColor = randomCode;
+
+    document.getElementById("color-code").innerHTML = randomCode
+
+    navigator.clipboard.writeText(randomCode)
+}
+
+//call on click
+document.getElementById("btn").addEventListener("click", getColor)
+
+
+//starting call without click
+getColor();
